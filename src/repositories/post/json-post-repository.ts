7ba53ac4c634a90fe3cf91c5792f69c331c7cf3a
Bruno Postai <cechinelbruno@gmail.com>
2026-03-10 15:10:29 +0100

    feature(repositories/post) repository pattern to posts

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,14 +1,40 @@
-import { PostModel } from "../../models/posts/post-model";
+import { resolve } from "path";
+import { PostModel } from "@/models/posts/post-model";
 import { PostRepository } from "./post-repository";
+import { readFile } from "fs/promises";
 
 const ROOT_DIR = process.cwd();
+const JSON_POST_FILE_PATH = resolve(
+  ROOT_DIR,
+  "src",
+  "db",
+  "seed",
+  "posts.json",
+);
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POST_FILE_PATH, "utf-8");
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
 
-  async findAll(): Promise<PostModel[]> {}
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) throw new Error("Post was not found");
+
+    return post;
+  }
 }
 
-export const postRepository = new JsonPostRepository();
+export const postRepository: PostRepository = new JsonPostRepository();
 
-console.log("HELLO WORLD");
+// npx tsx src\repositories\post\json-post-repository.ts
