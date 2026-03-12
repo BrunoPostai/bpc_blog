d24b6e00196f4e8bb75f5166967816f32c5d37f8
Bruno Postai <cechinelbruno@gmail.com>
2026-03-12 16:01:08 +0100

    feature(components/postslist) impletementing spinloader with suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import { Suspense } from "react";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <div>
-      <SpinLoader/>
+      <header className="text-6xl font-bold text-center py-8">
+        Aqui fica a header
+      </header>
+      <Suspense fallback={<SpinLoader className="h-screen"/>}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <p>FOOTER</p>
+      </footer>
     </div>
   );
 }
