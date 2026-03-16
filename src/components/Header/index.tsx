2f631151e08819fe24f3f4c58f53baeb1d70d836
Bruno Postai <cechinelbruno@gmail.com>
2026-03-16 08:41:04 +0100

    feature(components/header) adding breakpoins to header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,18 @@
-'use client';
-
-import clsx from 'clsx';
+import clsx from "clsx";
 
 export function Header() {
-  console.log('HEADER');
+  console.log("HEADER");
 
   return (
     <h1
       className={clsx(
-        'text-6xl',
-        'font-bold',
-        'text-blue-500',
-        'hover:text-blue-50',
-        'hover:bg-blue-500',
-        'transition',
-        'duration-1000',
+        "text-4xl/normal font-extrabold py-8",
+        "sm:text-5xl/normal sm:py-10",
+        "md:text-6xl/normal md:py-11",
+        "lg:text-7xl/normal lg:py-12",
       )}
-      onClick={() => alert(123)}
     >
-      HELLO WORLD
+      <a href="#">The Blog</a>
     </h1>
   );
 }
