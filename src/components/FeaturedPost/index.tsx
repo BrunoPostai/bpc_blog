f4122ec60b94a57d1a64fdb6d91f11a39205086d
Bruno Postai <cechinelbruno@gmail.com>
2026-03-19 23:32:00 +0100

    feature(react cache)  using react cache to remove double API calls

--- src/components/FeaturedPost/index.tsx
+++ src/components/FeaturedPost/index.tsx
@@ -1,10 +1,12 @@
 import { PostCoverImage } from "../PostCoverImage";
-import { PostHeading } from "../PostHeading";
 import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "@/lib/post/queries";
 
-export function FeaturedPost() {
-  const slug = "qualquer";
-  const postLink = `/post/${slug}`;
+export async function FeaturedPost() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+
+  const postLink = `/post/${post.slug}`;
 
   return (
     <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
@@ -15,19 +17,17 @@ export function FeaturedPost() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: "/images/bryen_9.png",
-          alt: "Alt da imagem",
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
       <PostSummary
         postLink={postLink}
         postHeading="h1"
-        createdAt={"2025-04-08T00:24:38.616Z"}
-        excerpt={
-          "O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO."
-        }
-        title={"Rotina matinal de pessoas altamente eficazes"}
+        createdAt={post.createdAt}
+        excerpt={post.excerpt}
+        title={post.title}
       />
     </section>
   );
