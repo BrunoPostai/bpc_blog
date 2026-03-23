702ae6b616d6130a82639b777026324ba6c3f7cb
Bruno Postai <cechinelbruno@gmail.com>
2026-03-24 00:54:52 +0100

    feature(post/slug) using generateMetadata to dinamically set the metadata

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,21 @@ import clsx from "clsx";
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        "min-h-90 bg-slate-900 text-slate-100",
-        "mb-16 p-8 rounded-xl",
-        "flex items-center justify-center",
-        "text-center",
-      )}
-    >
-      <div>
-        <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-        <p>404 ERROR - THE PAGE YOU'RE TRYING TO FIND DOES NOT EXIST</p>
+    <>
+      <title>PAGE NOT FOUND</title>
+      <div
+        className={clsx(
+          "min-h-90 bg-slate-900 text-slate-100",
+          "mb-16 p-8 rounded-xl",
+          "flex items-center justify-center",
+          "text-center",
+        )}
+      >
+        <div>
+          <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
+          <p>404 ERROR - THE PAGE YOU'RE TRYING TO FIND DOES NOT EXIST</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
