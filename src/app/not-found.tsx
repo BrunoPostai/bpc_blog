7dac6ff2ac1fd21b148225596f57d6788559d876
Bruno Postai <cechinelbruno@gmail.com>
2026-03-23 05:31:36 +0100

    feature(not-found.tsx) not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+
+export default function NotFoundPage() {
+  return (
+    <div
+      className={clsx(
+        "min-h-80 bg-slate-900 text-slate-100",
+        "mb-16 p-8 rounded-xl",
+        "flex items-center justify-center",
+        "text-center"
+      )}
+    >
+      <div>
+        <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
+        <p>404 ERROR - THE PAGE YOU'RE TRYING TO FIND DOES NOT EXIST</p>
+      </div>
+    </div>
+  );
+}
