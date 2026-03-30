4a5936605d1d711d8d8d9309b8f1e2332dfaed57
Bruno Postai <cechinelbruno@gmail.com>
2026-03-30 02:41:16 +0200

    feature(components/errormessage) error message component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,13 @@
-import clsx from "clsx";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export default function NotFoundPage() {
   return (
     <>
-      <title>PAGE NOT FOUND</title>
-      <div
-        className={clsx(
-          "min-h-90 bg-slate-900 text-slate-100",
-          "mb-16 p-8 rounded-xl",
-          "flex items-center justify-center",
-          "text-center",
-        )}
-      >
-        <div>
-          <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-          <p>404 ERROR - THE PAGE YOU'RE TRYING TO FIND DOES NOT EXIST</p>
-        </div>
-      </div>
+      <ErrorMessage
+        pageTitle="PAGE NOT FOUND"
+        contentTitle="404"
+        content="404 ERROR - THE PAGE YOU'RE TRYING TO FIND DOES NOT EXIST"
+      />
     </>
   );
 }
