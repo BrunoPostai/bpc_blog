b573042483bef74fdcc118ff19df53140b1fe0f0
Bruno Postai <cechinelbruno@gmail.com>
2026-04-03 13:20:31 +0200

    feature(db/seed) populating the db with seed.ts

---
+++ src/db/drizzle/seed.ts
@@ -0,0 +1,17 @@
+import { JsonPostRepository } from "@/repositories/post/json-post-repository";
+import { drizzleDb } from ".";
+import { postsTable } from "./schemas";
+
+(async () => {
+  const jsonPostRepository = new JsonPostRepository();
+  const posts = await jsonPostRepository.findAll();
+
+  try {
+    await drizzleDb.delete(postsTable); // DON'T FORGET
+    await drizzleDb.insert(postsTable).values(posts);
+  } catch (e) {
+    console.log();
+    console.log("Ocorreu um erro....", e);
+    console.log();
+  }
+})();
