e88d43975468738ea27d22e9cac39ad68a000a19
Bruno Postai <cechinelbruno@gmail.com>
2026-05-26 19:08:43 +0200

    feature(admin) setting up adming routes

---
+++ src/app/admin/post/[id]/page.tsx
@@ -0,0 +1,12 @@
+type AdminPostIdPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function AdminPostIdPage({
+  params,
+}: AdminPostIdPageProps) {
+  const { id } = await params;
+  return <div className="py-16 text-6xl">ADMIN POST ID {id}</div>;
+}
